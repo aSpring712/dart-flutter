6e299fb1b90f13d94475d3f96a2fbc02325b28b4
aSpring712 <rnjsqhal2001@naver.com>
2023-12-04 09:34:25 +0900

    dart로 문장 출력

--- class_01/lib/main.dart
+++ class_01/lib/main.dart
@@ -1,5 +1,15 @@
-import 'package:flutter/material.dart';
-
+// 코드의 시작점
+// dart 문자열에 대한 이해
 void main() {
+
+  String name = 'Dart'; // dart 홑따옴표, 쌍따옴표 모두 사용 가능! 하지만 홑따옴표를 권장
+  print(name);
+
+  String schoolName = "Diamond School";
+  String address = "New York 2040";
+
+  // 두 개의 변수를 화면에 출력
+  // ${} -> obect.key 가능
+  print("School name is $schoolName and address is ${address}");
 
 }
