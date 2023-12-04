865b1aea549fdfd46e65a9d3fb87d818bcdaf476
aSpring712 <rnjsqhal2001@naver.com>
2023-12-04 09:46:30 +0900

    bool 타입

---
+++ class_01/lib/main3.dart
@@ -0,0 +1,12 @@
+// bool - true or false을 저장하는데 사용됨
+void main() {
+
+  // bool 또한 객체
+  bool isTrue = true; // 객체 선언과 동시에 초기화
+  print(isTrue);
+
+  bool isMarried = false;
+  print('Married Status : ${isMarried}');
+
+  
+}
