e610fc68718e0bb562b1a2f4452cec35b568a161
aSpring712 <rnjsqhal2001@naver.com>
2023-12-05 09:18:24 +0900

    강제 형 변환

--- class_01/lib/main10.dart
+++ class_01/lib/main10.dart
@@ -1,7 +1,19 @@
 void main() {
 
-  int myInt = 42;
-  double myDouble = myInt; //int를 double로 자동 형변환
-  print(myDouble); // 출력: 42.0
+  double doubleValue = 3.14;
+  int intValue = doubleValue.toInt(); // 강제 형 변환 : 주의점 - 소수 부분은 잘리게 된다.
+  // 강제 형 변환 : 컴파일러에게 소수 부분은 잘라도 되니까 그냥 넣어버려 하는 것
+  print(intValue); // 3
 
+  int intValue2 = 33;
+  // double doubleValue2 = intValue2; // 자동 형 변환 안된
+  double doubleValue2 = intValue2.toDouble(); // 강제 형 변환
+
+  print(doubleValue2); // 33.0
+  print(doubleValue2.runtimeType); // double
+
+  print("---------------------------");
+  String strValue1 = doubleValue2.toString(); // 문자열로 강제 형변환은 toString() 사용
+  print(strValue1);
+  print(strValue1.runtimeType);
 }
