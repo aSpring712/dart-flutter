7e6a43323a54872faa49830496b505e3589a9fa7
aSpring712 <rnjsqhal2001@naver.com>
2023-12-05 10:20:54 +0900

    비교(관계) 연산자

---
+++ class_01/lib/main15.dart
@@ -0,0 +1,13 @@
+void main() {
+  int score = 90;
+
+  //   비교(관계) 연산자
+  // 관계 연산자를 주어로 생각하고 연산하기
+  // 10 < 100
+  if( score >= 90) {
+    print("A학점");
+  } else if ( score >= 80) {
+    print("B학점");
+  }
+
+}
