84fd4ff5bd8e13e6809834df0cb20ca6e5f7689f
aSpring712 <rnjsqhal2001@naver.com>
2023-12-05 09:21:24 +0900

    강제 형 변환. as

--- class_01/lib/main10.dart
+++ class_01/lib/main10.dart
@@ -16,4 +16,11 @@ void main() {
   String strValue1 = doubleValue2.toString(); // 문자열로 강제 형변환은 toString() 사용
   print(strValue1);
   print(strValue1.runtimeType);
+  // as 연산자 사용해보기
+
+  int intValue3 = 88;
+  double doubleValue3 = intValue3 as double; // 컴파일 시점에 오류가 발생하지 않는다.
+  // 실패함!!!
+  print(doubleValue3); // 런타임 시점에 오류가 남 "type 'int' is not a subtype of type 'double' in type cast" // int, double 타입은 상속 관계(부모자식)가 아니기 때문에 as 사용 불가
+
 }
