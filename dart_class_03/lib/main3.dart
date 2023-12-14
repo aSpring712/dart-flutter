c2211490a50060a5c7ec96c85e03c70088d7a95e
aSpring712 <rnjsqhal2001@naver.com>
2023-12-14 10:15:21 +0900

    값을 집어넣었을 때

--- dart_class_03/lib/main3.dart
+++ dart_class_03/lib/main3.dart
@@ -1,7 +1,7 @@
 void main() {
 
   // 1.
-  String? maybeName;
-  int length = maybeName?.length ?? 0; // null check 연산자 ? |||  ?? null이 아니면 문자열 길이 출력, null이면 0을 반환함
+  String? maybeName = "홍길동";
+  int length = maybeName.length; // null check 연산자 ? |||  ?? null이 아니면 문자열 길이 출력, null이면 0을 반환함
   print(length);
 }
