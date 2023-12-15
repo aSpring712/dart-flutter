17aed38e33ae1c403dbb34dc1d64d28b28d48a3d
aSpring712 <rnjsqhal2001@naver.com>
2023-12-15 09:24:32 +0900

    async, await로 동기적 통신

--- dart_class_03/lib/main7.dart
+++ dart_class_03/lib/main7.dart
@@ -1,4 +1,6 @@
-void main() {
+// Future 타입을 소화시키는 2가지 방법
+// 1. async, await
+void main() async {
 
   // Future 타입을 선언해 보자.
 
@@ -9,7 +11,11 @@ void main() {
   Future<int> number = Future.value(100);
   Future<bool> isTrue = Future.value(true);
 
-  print(name); // Instance of 'Future<String>'
-  print(number); // Instance of 'Future<int>'
-  print(isTrue); // Instance of 'Future<bool>'
+  // print(name); // Instance of 'Future<String>'
+  // print(number); // Instance of 'Future<int>'
+  // print(isTrue); // Instance of 'Future<bool>'
+
+  print(await name); // 텐코딩
+  print(await number); // 100
+  print(await isTrue); // true
 }
