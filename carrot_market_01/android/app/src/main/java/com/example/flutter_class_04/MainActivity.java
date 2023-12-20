60d34d71822967bf8e62a8aa36f459750a31abd4
aSpring712 <rnjsqhal2001@naver.com>
2023-12-20 09:22:45 +0900

    fordering

---
+++ carrot_market_01/android/app/src/main/java/com/example/flutter_class_04/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutter_class_04;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ carrot_market_01/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CarrotMarketUI());
+}
+
+class CarrotMarketUI extends StatelessWidget {
+  const CarrotMarketUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
