fb36da49ebff089fb6204af288079068d872b1ba
aSpring712 <rnjsqhal2001@naver.com>
2023-12-20 10:53:48 +0900

    이미지 노출

--- carrot_market_01/lib/screens/home/home_screen.dart
+++ carrot_market_01/lib/screens/home/home_screen.dart
@@ -1,12 +1,46 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_class_04/models/Product.dart';
+import 'package:flutter_class_04/screens/home/product_item.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatelessWidget { // 한 번 렌더링하면 상호작용이 일어나도 다시 렌더링 할 필요없음
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Row(
+          children: [
+            Text('좌동'),
+            SizedBox(width: 4.0), // 이만큼 공간을 차지하라
+            Icon(
+              CupertinoIcons.chevron_down, size: 15.0,
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.search)),
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.list_dash)),
+        ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(0.5),
+          child: Divider(thickness: 0.5, height: 0.5, color: Colors.grey,),
+        ),
+      ),
+      body: ListView.separated(
+          itemBuilder: (context, index) {
+            // productList[index] --> 0 : product();
+            return ProductItem(product: productList[index]);
+          },
+          separatorBuilder: (context, index) => Divider( // 각 아이템 마다 구분을 시킬 수 있음
+            height: 0,
+            indent: 16,
+            endIndent: 16,
+            color: Colors.grey,
+          ),
+          itemCount: productList.length
+      ),
     );
   }
 }
---
+++ carrot_market_01/lib/screens/home/product_item.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_class_04/models/Product.dart';
+import 'package:flutter_class_04/screens/home/product_detail.dart';
+
+class ProductItem extends StatelessWidget {
+
+  // 규칙1. const 생성자는 초기화 되어야 하는 변수는 반드시 상수 값이 되어야 한다.
+  // 상수 -> const(compile 시점에 상수화), final
+  /* const Product product; // -> error -> compile 시점에 전달받지 못함(메모리에 올라가야 전달받을 수 있음) -> 메모리가 실행되는 시점에 전달받을 수 있도록 final
+  * */
+  final Product product;
+  const ProductItem({required this.product, super.key}); // 생성자 의존 주입
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect( // 모서리 둥글게
+      borderRadius: BorderRadius.circular(10.0),
+      child: Container(
+        height: 135.0,
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            // Image.asset(name) // local에 있는 이미지를 올리는 것
+            Image.network( // 통신을 통해 받아올 때 쓰는 것
+              product.urlToImage,
+              width: 115,
+              height: 115,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(width: 16.0),
+            ProductDetail(),
+          ],
+        ),
+      ),
+    );
+  }
+}
