5a668cb52e5fa15ec49b99f0f0f5fdb8559df9e2
aSpring712 <rnjsqhal2001@naver.com>
2023-12-29 09:48:09 +0900

    callback 함수 설계

--- class_callback/lib/main.dart
+++ class_callback/lib/main.dart
@@ -16,6 +16,15 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String childMessageContent = "여기는 부모 위젯 영역이야";
 
+  // 매개변수로 자식들에게 함수를 전달
+  void onCallbackPressed() {
+    print("1111");
+    // stf 위젯에 화면을 렌더링 하려면 --> setState() 함수를 써야 함
+    setState(() {
+      childMessageContent = "자식에게 이벤트 발생 했네";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,8 +35,12 @@ class _MyAppState extends State<MyApp> {
               Expanded(
                   child: Center(child: Text(childMessageContent))
               ),
-              Expanded(flex: 1, child: ChildA()), // Expanded : 확장
-              Expanded(flex: 1, child: ChildB()),
+              Expanded(flex: 1, child: ChildA(
+                callback: onCallbackPressed,
+              )), // Expanded : 확장
+              Expanded(flex: 1, child: ChildB(
+                callback: onCallbackPressed,
+              )),
             ],
           ),
         ),
@@ -36,18 +49,23 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-// 자식
+// 자식 -> 반드시 부모 위젯이 있어야 자식이 있을 수 있다(종속성)
 class ChildA extends StatelessWidget {
-  const ChildA({super.key});
+
+  // 상태, 기능
+  final VoidCallback callback;
+
+  const ChildA({
+    required this.callback, // 생성자 조건 변경
+    super.key
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: InkWell(
-        onTap: () {
-          print("child A에 이벤트 발생");
-      },
+        onTap: callback, // event 발생 시 자식 -> 부모
       child: Container(
         width: double.infinity, // 영역만큼 무한히 넓혀라
         color: Colors.orange,
@@ -60,16 +78,19 @@ class ChildA extends StatelessWidget {
 
 // 자식
 class ChildB extends StatelessWidget {
-  const ChildB({super.key});
+  final VoidCallback callback;
+
+  const ChildB({required this.callback, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: InkWell(
-        onTap: () {
-          print("child B에 이벤트 발생");
-        },
+        onTap: callback,
+        // onTap: () {
+        //   print("child B에 이벤트 발생");
+        // },
         child: Container(
           width: double.infinity, // 영역만큼 무한히 넓혀라
           color: Colors.red,
