9ce44a899fd430f3cc69a3e0f8d669d8639902e1
aSpring712 <rnjsqhal2001@naver.com>
2024-01-09 10:35:58 +0900

    riverpod 4. 상태 관리 활용(f_catalog.dart까지 완료)

--- class_statement/lib/riverpod/f_catalog.dart
+++ class_statement/lib/riverpod/f_catalog.dart
@@ -1,10 +1,12 @@
 // 전체 화면에서 어디서든 쓸 수 있는 조각 -> flagment
 import 'package:class_statement/common/w_catalog_item.dart';
+import 'package:class_statement/riverpod/state/riverpod_cart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../common/models/vo_catalog.dart';
 
-class CatalogWidget extends StatelessWidget {
+class CatalogWidget extends ConsumerWidget {
 
   // // 통신을 통해 받은 데이터가 필요함
   // final List<Catalog> responseListData;
@@ -20,8 +22,8 @@ class CatalogWidget extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ListView.builder( /// 통신으로 받아서 화면 전체에 list를 뿌려주는 것
         // catalogListSample -> lib/models/vo_catalog.dart의 전역 변수
         itemCount: catalogListSample.length, // 만큼 반복을 돌릴 것
         itemBuilder: (context, index) {
@@ -31,8 +33,10 @@ class CatalogWidget extends StatelessWidget {
           return CatalogItem(
               catalog: catalog,
               // TODO 임시 수정 (코드 변경 예정)
-              isInCart: false, // 정보는 catalog_item이 가지고 있음
-              onPressedCatalog: (catalog) {});
+              // ref.watch(cartProvider) -> List
+              // ref.read(cartProvider.notifier) -> 상태 클래스에 접근 (RiverpodCart)
+              isInCart: ref.watch(cartProvider).contains(catalog), // true or false
+              onPressedCatalog: ref.read(cartProvider.notifier).onCatalogPressed);
         },
     );
   }
